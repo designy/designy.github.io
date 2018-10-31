ff338f98577be25bb9146dafe01ecc9eb28caab1
ali esmaeili <a.esmaeili.sut@gmail.com>
2018-10-31 18:26:16 +0330

    fix

--- najva-messaging-sw.js
+++ najva-messaging-sw.js
@@ -1,3 +1,1 @@
-var today = new Date();
-var todayString = "?v=" + today.getFullYear() + ("0" + today.getMonth()).slice(-2) + ("0" + today.getUTCDate()).slice(-2);
-importScripts('https://app.najva.com/static/cdn/https_worker/najva-messaging-sw.v2.js' + todayString);
+importScripts('https://app.najva.com/static/cdn/https_worker/najva-messaging-sw.v2.js');
