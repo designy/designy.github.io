279eefb1068a8dcb053c3a285862c0c709c2e5b5
ali esmaeili <a.esmaeili.sut@gmail.com>
2018-10-31 14:05:28 +0330

    fix

--- najva-messaging-sw.js
+++ najva-messaging-sw.js
@@ -1,1 +1,1 @@
-importScripts('https://app.najva.com/static/cdn/https_worker/najva-messaging-sw.v2.js');
+importScripts('https://app.najva.com/static/cdn/https_worker/najva-messaging-sw.v2.js?v=12');
