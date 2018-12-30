ea8359897e027f662e449df4eb69d15cb557def2
ali esmaeili <a.esmaeili.sut@gmail.com>
2018-12-30 10:26:13 +0330

    git push

--- najva-messaging-sw.js
+++ najva-messaging-sw.js
@@ -53,7 +53,6 @@ self.addEventListener('notificationclick', function (event) {
 
     event.waitUntil(
         clients.matchAll({
-            type: "window",
             includeUncontrolled: true
         })
             .then(function (clientList) {
@@ -133,7 +132,7 @@ self.onnotificationclick = function(event) {
   // This looks to see if the current is already open and
   // focuses if it is
   event.waitUntil(clients.matchAll({
-    type: "window",includeUncontrolled: true
+      includeUncontrolled: true
   }).then(function(clientList) {
     for (var i = 0; i < clientList.length; i++) {
       var client = clientList[i];
