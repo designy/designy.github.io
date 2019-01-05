1c7ae584370ab6b2e69e47214a55e3d75fc1b53f
ali esmaeili <a.esmaeili.sut@gmail.com>
2019-01-05 18:21:40 +0330

    add amp link

--- najva-messaging-sw.js
+++ najva-messaging-sw.js
@@ -87,3 +87,327 @@ self.addEventListener('notificationclose', function (event) {
 });
 
 
+/**
+ * Copyright 2017 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+/** @fileoverview
+  This file is an example implementation for a service worker compatible with
+  amp-web-push. This means the service worker accepts window messages (listened
+  to via the service worker's 'message' handler), performs some action, and
+  replies with a result.
+
+  The service worker listens to postMessage() messages sent from a lightweight
+  invisible iframe on the canonical origin. The AMP page sends messages to this
+  "helper" iframe, which then forwards the message to the service worker.
+  Broadcast replies from the service worker are received by the helper iframe,
+  which broadcasts the reply back to the AMP page.
+ */
+
+/** @enum {string} */
+const WorkerMessengerCommand = {
+  /*
+    Used to request the current subscription state.
+   */
+  AMP_SUBSCRIPION_STATE: 'amp-web-push-subscription-state',
+  /*
+    Used to request the service worker to subscribe the user to push.
+    Notification permissions are already granted at this point.
+   */
+  AMP_SUBSCRIBE: 'amp-web-push-subscribe',
+  /*
+    Used to unsusbcribe the user from push.
+   */
+  AMP_UNSUBSCRIBE: 'amp-web-push-unsubscribe',
+};
+
+/*
+  According to
+  https://w3c.github.io/ServiceWorker/#run-service-worker-algorithm:
+
+  "user agents are encouraged to show a warning that the event listeners
+  must be added on the very first evaluation of the worker script."
+
+  We have to register our event handler statically (not within an
+  asynchronous method) so that the browser can optimize not waking up the
+  service worker for events that aren't known for sure to be listened for.
+
+  Also see: https://github.com/w3c/ServiceWorker/issues/1156
+*/
+self.addEventListener('message', event => {
+  /*
+    Messages sent from amp-web-push have the format:
+
+    - command: A string describing the message topic (e.g.
+      'amp-web-push-subscribe')
+
+    - payload: An optional JavaScript object containing extra data relevant to
+      the command.
+   */
+  const {command} = event.data;
+
+  switch (command) {
+    case WorkerMessengerCommand.AMP_SUBSCRIPION_STATE:
+      onMessageReceivedSubscriptionState();
+      break;
+    case WorkerMessengerCommand.AMP_SUBSCRIBE:
+      onMessageReceivedSubscribe();
+      break;
+    case WorkerMessengerCommand.AMP_UNSUBSCRIBE:
+      onMessageReceivedUnsubscribe();
+      break;
+  }
+});
+
+/**
+  Broadcasts a single boolean describing whether the user is subscribed.
+ */
+function onMessageReceivedSubscriptionState() {
+  let retrievedPushSubscription = null;
+  self.registration.pushManager.getSubscription()
+      .then(pushSubscription => {
+        retrievedPushSubscription = pushSubscription;
+        if (!pushSubscription) {
+          return null;
+        } else {
+          return self.registration.pushManager.permissionState(
+              pushSubscription.options
+          );
+        }
+      }).then(permissionStateOrNull => {
+        if (permissionStateOrNull == null) {
+          broadcastReply(WorkerMessengerCommand.AMP_SUBSCRIPION_STATE, false);
+        } else {
+          const isSubscribed = !!retrievedPushSubscription &&
+            permissionStateOrNull === 'granted';
+          broadcastReply(WorkerMessengerCommand.AMP_SUBSCRIPION_STATE,
+              isSubscribed);
+        }
+      });
+}
+
+/**
+  Subscribes the visitor to push.
+
+  The broadcast value is null (not used in the AMP page).
+ */
+function onMessageReceivedSubscribe() {
+  /*
+    If you're integrating amp-web-push with an existing service worker, use your
+    existing subscription code. The subscribe() call below is only present to
+    demonstrate its proper location. The 'fake-demo-key' value will not work.
+
+    If you're setting up your own service worker, you'll need to:
+      - Generate a VAPID key (see:
+        https://developers.google.com/web/updates/2016/07/web-push-interop-wins)
+      - Using urlBase64ToUint8Array() from
+        https://github.com/web-push-libs/web-push, convert the VAPID key to a
+        UInt8 array and supply it to applicationServerKey
+   */
+              window.najvaSettings = {
+                campaign_id: "673",
+                without_popup: false,
+                with_popup: true,
+                api_key: "33b205837e294262bb3a3955a9b0f906",
+                najva_subdomain: "https://aliii153.najva.com/",
+                website_id: "167",
+                location_permission: true,
+                request_text: "آیا میخواهید پربازدیدترین موضوعات برای شما ارسال شود؟",
+                request_description: "تخفیف‌ها، آموزش ها و جدیدترین اخبار",
+                accept_text: "بله",
+                denied_text: "خیر",
+                request_icon: "http://127.0.0.1:8009/static/media/upload/request_icon/240_F_188168883_DIrtPjMyDWWlsKBt3plSc1zHhO3VIOcM_sekxjbN.jpg",
+
+
+                dismiss_cookie_days: 3,
+                request_permission: {
+                    delay: {
+                        enable: 1,
+                        value: 1,
+                    },
+                    scroll: {
+                        enable: 0,
+                        value: 50,
+                    },
+                    visit: {
+                        minimum: {
+                            enable: 1,
+                            value: 2,
+                        },
+                        maximum: {
+                            enable: 0,
+                            value: 24,
+                        },
+                        interval: {
+                            enable: 0,
+                            value: 3,
+                        },
+                    },
+                },
+                show_bell: 1,
+                show_bell_in_mobile: 1,
+                bell_direction: "right",
+                tooltip_direction: "right",
+                bell_color_hover: "#7a1002",
+                bell_color: "#35c91b",
+                bell_tooltip: "مشترک شوید",
+                };
+  function sendTokenToServer(token) {
+        var http = new XMLHttpRequest();
+        var url = "{{ host_url }}api/v1/add/";
+        var params = "token_id=" + token + "&topic=" + najvaSettings.campaign_id + "&website_id=" + najvaSettings.website_id + "&api_key=" + najvaSettings.api_key;
+        http.withCredentials = true;
+        http.open("POST", url, true);
+        http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+        http.onreadystatechange = function () {
+            if (http.readyState == 4) {
+                if (http.status == 200) {
+                    cookie_token = JSON.parse(http.response).cookie_token;
+                    if (cookie_token) {
+                        if (window.najvaUserSubscribed) {
+                            window.najvaUserSubscribed(cookie_token)
+                        }
+                    }
+                    // if (location_permission) {
+                    //     // get location permission
+                    //     navigator.permissions.query({ name: 'geolocation' }).then(function (permission) {
+                    //         console.log("initial permission state : ", permission.state);
+                    //         var options = {
+                    //             enableHighAccuracy: true,
+                    //             timeout: 60000,
+                    //             maximumAge: 0
+                    //         };
+                    //
+                    //         function success(pos) {
+                    //             var crd = pos.coords;
+                    //             var data = {
+                    //                 "accuracy": crd.accuracy,
+                    //                 "altitude": crd.altitude,
+                    //                 "latitude": crd.latitude,
+                    //                 "longitude": crd.longitude
+                    //             };
+                    //             var url = "{% url 'set_subscriber_location' %}";
+                    //             sendRequest(url, function success(response) {
+                    //                 setCookie("tlc", true, 0.5)
+                    //                 console.log("close8")
+                    //                 setTimeout(function () {
+                    //                     window.close();
+                    //                 }, 2000);
+                    //
+                    //             }, function error(response) {
+                    //                 console.log("close9")
+                    //                 setTimeout(function () {
+                    //                     window.close();
+                    //                 }, 2000);
+                    //             }, "POST", JSON.stringify(data), { "Content-Type": "application/json" });
+                    //
+                    //
+                    //             /*
+                    //             .catch(function () {
+                    //
+                    //                 console.log("unexpected error");
+                    //                 setTimeout(
+                    //                     function () {
+                    //                     window.close();
+                    //                 }, 5000);
+                    //             });
+                    //             */
+                    //         }
+                    //
+                    //         function error(err) {
+                    //             console.log("close12")
+                    //             setTimeout(function () {
+                    //                 window.close();
+                    //             }, 2000);
+                    //             console.warn(`ERROR(${err.code}): ${err.message}`);
+                    //         }
+                    //
+                    //         navigator.geolocation.getCurrentPosition(success, error, options);
+                    //
+                    //         permission.onchange = function (event) {
+                    //             var state = event.currentTarget.state;
+                    //             console.log("permission status changed to : ", state);
+                    //             if (state == "granted") {
+                    //                 console.log("user granted location access.")
+                    //             }
+                    //             else if (state == "denied") {
+                    //                 console.log("user denied location access.")
+                    //             }
+                    //             else if (state == "prompt") {
+                    //                 navigator.geolocation.getCurrentPosition(success, error, options);
+                    //             }
+                    //         }
+                    //
+                    //     });
+                    // }
+
+
+                }
+            }
+        };
+
+        http.send(params);
+    }
+  messaging.getToken()
+    .then(currentToken => {
+        sendTokenToServer(currentToken, state);
+        broadcastReply(WorkerMessengerCommand.AMP_SUBSCRIBE, null);
+    })
+    .catch(err => {
+        console.error(err);
+    });
+  // self.registration.pushManager.subscribe({
+  //   userVisibleOnly: true,
+  //   applicationServerKey: 'fake-demo-key',
+  // }).then(() => {
+  //   // IMPLEMENT: Forward the push subscription to your server here
+  //   broadcastReply(WorkerMessengerCommand.AMP_SUBSCRIBE, null);
+  // });
+}
+
+
+/**
+  Unsubscribes the subscriber from push.
+
+  The broadcast value is null (not used in the AMP page).
+ */
+function onMessageReceivedUnsubscribe() {
+  self.registration.pushManager.getSubscription()
+      .then(subscription => subscription.unsubscribe())
+      .then(() => {
+        // OPTIONALLY IMPLEMENT: Forward the unsubscription to your server here
+        broadcastReply(WorkerMessengerCommand.AMP_UNSUBSCRIBE, null);
+      });
+}
+
+/**
+ * Sends a postMessage() to all window frames the service worker controls.
+ * @param {string} command
+ * @param {!JsonObject} payload
+ */
+function broadcastReply(command, payload) {
+  self.clients.matchAll()
+      .then(clients => {
+        for (let i = 0; i < clients.length; i++) {
+          const client = clients[i];
+          client./*OK*/postMessage({
+            command,
+            payload,
+          });
+        }
+      });
+}
+
