27814aad93a4ea1104fe3420c0bb2e49aa826653
ali esmaeili <a.esmaeili.sut@gmail.com>
2019-01-08 14:32:51 +0330

    add amp link

--- najva-messaging-sw.js
+++ najva-messaging-sw.js
@@ -10,6 +10,14 @@ firebase.initializeApp(config);
 
 const messaging = firebase.messaging();
 
+function isMobileUserAgent() {
+    let check = false;
+    (function (a) {
+        if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true;
+    })(self.navigator.userAgent || self.navigator.vendor);
+    return check;
+}
+
 messaging.setBackgroundMessageHandler(function (payload) {
     console.log(payload);
 
@@ -33,19 +41,24 @@ messaging.setBackgroundMessageHandler(function (payload) {
         sound: payload.data.sound,
     };
     let notif_actions = [];
-    if (payload.data.btn1_enabled) {
-        notif_actions.push({
-            title: payload.data.btn1_title,
-            icon: payload.data.btn1_icon,
-            action: "btn1_clicked",
-        });
+    const isMobile = isMobileUserAgent();
+    if (payload.data.btn1_enabled && payload.data.btn1_action) {
+        if (payload.data.btn1_action !== "open-sms" || isMobile) {
+            notif_actions.push({
+                title: payload.data.btn1_title,
+                icon: payload.data.btn1_icon,
+                action: "btn1_clicked",
+            });
+        }
     }
-    if (payload.data.btn2_enabled) {
-        notif_actions.push({
-            title: payload.data.btn2_title,
-            icon: payload.data.btn2_icon,
-            action: "btn2_clicked",
-        });
+    if (payload.data.btn2_enabled && payload.data.btn2_action) {
+        if (payload.data.btn2_action !== "open-sms" || isMobile) {
+            notif_actions.push({
+                title: payload.data.btn2_title,
+                icon: payload.data.btn2_icon,
+                action: "btn2_clicked",
+            });
+        }
     }
     notificationOptions["actions"] = notif_actions;
 
@@ -78,36 +91,14 @@ self.addEventListener('notificationclick', function (event) {
             })
                 .then(function (clientList) {
                     const action = event.notification.data.btn1_action;
-
-                    if (action === "open-link") {
-                        if (clients.openWindow) {
-                            return clients.openWindow(event.notification.data.btn1_final_address);
-                        }
+                    const final_address = event.notification.data.btn1_final_address;
+                    const content = event.notification.data.btn1_content;
+                    let url = "https://app.najva.com/call-actions/?";
+                    url += `action=${action}&final_address=${final_address}&content=${content}`;
+                    if (clients.openWindow) {
+                        return clients.openWindow(url);
                     }
-                    else if (action === "open-call") {
-                        if (clients.openWindow) {
-                            return clients.openWindow("https://aliii.ir/b");
-                        }
-                    }
-                    else if (action === "open-sms") {
-
-                    }
-                    else if (action === "open-app") {
-
-                    }
-                    else if (action === "open-telegram-channel") {
-
-                    }
-                    else if (action === "join-telegram-channel") {
-
-                    }
-                }).then(function (WindowClient) {
-                console.log("window opened success")
-                WindowClient.focus()
-            }).catch(function (WindowClient) {
-                console.log("window catch error")
-                console.log(WindowClient)
-            })
+                })
         );
     }
     else if (event.action === "btn2_clicked") {
@@ -118,37 +109,15 @@ self.addEventListener('notificationclick', function (event) {
             })
                 .then(function (clientList) {
                     const action = event.notification.data.btn2_action;
-                    if (action === "open-link") {
-                        if (clients.openWindow) {
-                            return clients.openWindow(event.notification.data.btn2_final_address);
-                        }
+                    const final_address = event.notification.data.btn2_final_address;
+                    const content = event.notification.data.btn2_content;
+                    let url = "https://aliii.ir/b/?";
+                    url += `action=${action}&final_address=${final_address}&content=${content}`;
+                    if (clients.openWindow) {
+                        return clients.openWindow(url);
                     }
-                    else if (action === "open-call") {
-                        if (clients.openWindow) {
-                            clients.openWindow("https://aliii.ir/b");
-                        }
-                    }
-                    else if (action === "open-sms") {
-
-                    }
-                    else if (action === "open-app") {
-
-                    }
-                    else if (action === "open-telegram-channel") {
-
-                    }
-                    else if (action === "join-telegram-channel") {
-
-                    }
-                }).then(function (WindowClient) {
-                console.log("window opened success")
-                WindowClient.focus()
-            }).catch(function (WindowClient) {
-                console.log(WindowClient)
-                console.log("window catch error")
-            })
-    )
-        ;
+                })
+        );
     }
     else {
         event.notification.close();
