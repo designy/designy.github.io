18f248970325beb46e03e9bc4d28058495ca4ec6
ali esmaeili <a.esmaeili.sut@gmail.com>
2020-04-14 13:57:24 +0430

    add gif

--- assets/js/yektanet_category.js
+++ assets/js/yektanet_category.js
@@ -125,30 +125,11 @@ jQuery(document).ready(function() {
 		});
 	}
 
-	function catchPaste(evt, elem, callback) {
-	  if (navigator.clipboard && navigator.clipboard.readText) {
-		// modern approach with Clipboard API
-		navigator.clipboard.readText().then(callback);
-	  } else if (evt.originalEvent && evt.originalEvent.clipboardData) {
-		// OriginalEvent is a property from jQuery, normalizing the event object
-		callback(evt.originalEvent.clipboardData.getData('text'));
-	  } else if (evt.clipboardData) {
-		// used in some browsers for clipboardData
-		callback(evt.clipboardData.getData('text/plain'));
-	  } else if (window.clipboardData) {
-		// Older clipboardData version for Internet Explorer only
-		callback(window.clipboardData.getData('Text'));
-	  } else {
-		// Last resort fallback, using a timer
-		setTimeout(function() {
-		  callback(elem.value)
-		}, 100);
-	  }
-	}
 	jQuery("#catfeature__demo-input-url").bind('paste', function(e) {
-		catchPaste(e, this, function(clipData) {
+	setTimeout(function() {
 		 	jQuery('.catfeature__demo-submit').click()
-	  });
+    }, 500);
+
 	});
 	jQuery("#catfeature__demo-input-url").keypress(function(event){
 		var keycode = (event.keyCode ? event.keyCode : event.which);
