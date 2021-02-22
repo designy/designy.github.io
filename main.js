c01e73dc800ca33356272476f2927b9e67f3c801
ali esmaeili <a.esmaeili.sut@gmail.com>
2021-02-22 18:28:37 +0330

    add unit8

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
-const publicKey = new Uint8Array('BLGbajUFzGauVQ2rv3n_rM3mPkpvrSzzG7mJNG7UC33sDhXp7TLMwFzmIUCPFZnGfgKjG87NS8uV8Mej2t52uyI');
+const urlB64ToUint8Array = base64String => {
+  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, "+")
+    .replace(/_/g, "/");
+  const rawData = atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+};
+const publicKey = urlB64ToUint8Array('BLGbajUFzGauVQ2rv3n_rM3mPkpvrSzzG7mJNG7UC33sDhXp7TLMwFzmIUCPFZnGfgKjG87NS8uV8Mej2t52uyI');
 
 function subscribeUser() {
   if ('serviceWorker' in navigator) {
