f363f1874adeaae1fce01b6a6419901602054c57
TNT <a.esmaeili.sut@gmail.com>
2023-02-04 16:25:18 +0330

    revert nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,6 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-  router: {
-    base: '/designy.github.io/'
-  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'aliii',
